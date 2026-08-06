dc66dcd651142dfb74fdfb28719abf6d1b6434af
GiuTP <giuliannopinheirotavares@gmail.com>
2026-08-05 22:46:39 -0300

    feat: feito código de inicialização do bd

---
+++ cmd/bot/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"gfinancer/internal/storage"
+	"log"
+)
+
+func main() {
+	fmt.Println("Iniciano o Gerenciador Financeiro...")
+
+	dbPath := "./data/financas.db"
+
+	db, err := storage.InitDB(dbPath)
+	if err != nil {
+		log.Fatalf("Falha crítica ao inicializar o banco: %v\n", err)
+	}
+
+	defer db.Close()
+
+	fmt.Println("Banco de dados inicializado e tabelas criadas com sucesso em:", dbPath)
+}
