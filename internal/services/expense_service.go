2575ca747509186658ab5959354b0d9d759141db
GiuTP <giuliannopinheirotavares@gmail.com>
2026-08-29 16:26:09 -0300

    feat: implementação de report service (geração de relatórios em pdf)

--- internal/services/expense_service.go
+++ internal/services/expense_service.go
@@ -6,6 +6,7 @@ import (
 	"strconv"
 	"strings"
 	"time"
+	"unicode"
 
 	"database/sql"
 
@@ -50,7 +51,7 @@ func (s *ExpenseService) Create(msg string) (string, error) {
 	return fmt.Sprintf("Compra (ID %d) salva com sucesso!", newExpense.Id), nil
 }
 
-func (s *ExpenseService) List(msg string) (string, error) {
+func (s *ExpenseService) parseFilters(msg string) (int, int, uint, uint, error) {
 	now := time.Now()
 	month := int(now.Month())
 	year := int(now.Year())
@@ -63,55 +64,74 @@ func (s *ExpenseService) List(msg string) (string, error) {
 
 		if m, err := strconv.Atoi(tokens[0]); err == nil {
 			if m < 1 || m > 12 {
-				return "", errors.New("Mês inválido. Use de 1 a 12")
+				return 0, 0, 0, 0, errors.New("Mês inválido. Use de 1 a 12")
 			}
 			month = m
 		} else {
 			switch strings.ToLower(tokens[0]) {
 			case "p":
 				if len(tokens) < 2 {
-					return "", errors.New("Use: /lista e p [nome]")
+					return 0, 0, 0, 0, errors.New("Use: /lista e p [nome]")
 				}
 				pMap, err := s.pRepo.ReadMap()
 				if err != nil {
-					return "", fmt.Errorf("Falha ao carregar o mapa de pessoas: %w", err)
+					return 0, 0, 0, 0, fmt.Errorf("Falha ao carregar o mapa de pessoas: %w", err)
 				}
 				p, exists := pMap[strings.ToLower(tokens[1])]
 				if !exists {
-					return "", errors.New("Pessoa não encontrada. Use /lista p")
+					return 0, 0, 0, 0, errors.New("Pessoa não encontrada. Use /lista p")
 				}
 				personId = p.Id
 
 				if len(tokens) == 3 {
 					month, err = strconv.Atoi(tokens[2])
 					if err != nil || month < 1 || month > 12 {
-						return "", errors.New("MêS inválido. Use de 1 a 12")
+						return 0, 0, 0, 0, errors.New("MêS inválido. Use de 1 a 12")
 					}
 				}
 			case "c":
 				if len(tokens) < 2 {
-					return "", errors.New("Use: /lista e c [nome]")
+					return 0, 0, 0, 0, errors.New("Use: /lista e c [nome]")
 				}
 				cMap, err := s.cRepo.ReadMap()
 				if err != nil {
-					return "", fmt.Errorf("Falha ao carregar o mapa de cartões: %w", err)
+					return 0, 0, 0, 0, fmt.Errorf("Falha ao carregar o mapa de cartões: %w", err)
 				}
 				c, exists := cMap[strings.ToLower(tokens[1])]
 				if !exists {
-					return "", errors.New("Cartão não encontrada. Use /lista c")
+					return 0, 0, 0, 0, errors.New("Cartão não encontrada. Use /lista c")
 				}
 				cardId = c.Id
 
 				if len(tokens) == 3 {
 					month, err = strconv.Atoi(tokens[2])
 					if err != nil || month < 1 || month > 12 {
-						return "", errors.New("MêS inválido. Use de 1 a 12")
+						return 0, 0, 0, 0, errors.New("MêS inválido. Use de 1 a 12")
 					}
 				}
 			default:
-				return "", errors.New("Filtro inválido. Use um número de mês, ou 'p [nome]' / 'c [nome]'")
+				return 0, 0, 0, 0, errors.New("Filtro inválido. Use um número de mês, ou 'p [nome]' / 'c [nome]'")
 			}
 		}
+	}
+
+	return month, year, personId, cardId, nil
+}
+
+func capitalize(s string) string {
+	if len(s) == 0 {
+		return s
+	}
+
+	r := []rune(s)
+	r[0] = unicode.ToUpper(r[0])
+	return string(r)
+}
+
+func (s *ExpenseService) List(msg string) (string, error) {
+	month, year, personId, cardId, err := s.parseFilters(msg)
+	if err != nil {
+		return "", fmt.Errorf("%w", err)
 	}
 
 	expenses, err := s.repo.Get(month, year, personId, cardId)
@@ -127,11 +147,80 @@ func (s *ExpenseService) List(msg string) (string, error) {
 	sb.WriteString(fmt.Sprintf("Compras (%02d/%02d): \n\n", month, year))
 
 	for _, e := range expenses {
-		valueStr := fmt.Sprintf("R$ %.2f", e.TotalValue)
-		sb.WriteString(fmt.Sprintf("- (%d) %s | %s\n", e.Id, e.Description, valueStr))
+		valueStr := fmt.Sprintf("R$ %.2f", e.InstallmentValue)
+
+		installStr := ""
+		if e.TotalInstallments > 1 {
+			installStr = fmt.Sprintf(" (%d/%d)", e.CurrentInstallment, e.TotalInstallments)
+		}
+		sb.WriteString(fmt.Sprintf("- %s%s | %s\n", e.Description, installStr, valueStr))
 	}
 
 	return sb.String(), nil
+}
+
+func (s *ExpenseService) GetReportData(msg string) (string, []domain.Report, string, error) {
+	month, year, personId, cardId, err := s.parseFilters(msg)
+	if err != nil {
+		return "", nil, "", err
+	}
+
+	expenses, err := s.repo.Get(month, year, personId, cardId)
+	if err != nil {
+		return "", nil, "", fmt.Errorf("Falha ao busca no banco de dados: %w", err)
+	}
+	if len(expenses) == 0 {
+		return "", nil, "", fmt.Errorf("Nenhuma fatura encontrada para %02d/%04d", month, year)
+	}
+
+	pMap, _ := s.pRepo.ReadMap()
+	cMap, _ := s.cRepo.ReadMap()
+
+	pById := make(map[uint]string)
+	for name, p := range pMap {
+		pById[p.Id] = name
+	}
+
+	cById := make(map[uint]string)
+	for name, c := range cMap {
+		cById[c.Id] = name
+	}
+
+	var reports []domain.Report
+	var total float64
+
+	for _, e := range expenses {
+		total += float64(e.InstallmentValue)
+
+		installStr := ""
+		if e.TotalInstallments > 1 {
+			installStr = fmt.Sprintf(" (%d/%d)", e.CurrentInstallment, e.TotalInstallments)
+		}
+
+		personName := "N/A"
+		if e.PersonId != nil {
+			personName = capitalize(pById[*e.PersonId])
+		}
+
+		cardName := "N/A"
+		if e.CardId != nil {
+			cardName = capitalize(cById[*e.CardId])
+		}
+
+		reports = append(reports, domain.Report{
+			Date:        e.DueDate,
+			Description: e.Description,
+			Value:       fmt.Sprintf("R$ %.2f", e.InstallmentValue),
+			Installment: installStr,
+			Person:      personName,
+			Card:        cardName,
+		})
+	}
+
+	title := fmt.Sprintf("EXTRATO - %02d/%04d", month, year)
+	totalStr := fmt.Sprintf("R$ %.2f", total)
+
+	return title, reports, totalStr, nil
 }
 
 func (s *ExpenseService) Update(msg string) (string, error) {
